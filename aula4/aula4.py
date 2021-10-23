23862aad31c3dd4172639b0705cc598355238463
arturnd <arturrepresentacao@gmail.com>
2021-10-23 17:46:35 -0300

    Arquivo aula4.py editada

--- aula4/aula4.py
+++ aula4/aula4.py
@@ -13,3 +13,14 @@ n1=3;n2=6;x=complex(n1,n2); print(type(x))#complexo
 print("Parte Real: ",(x.real));print('Parte Imag: ',(x.imag))
 x=["carro","navio","aviao","trem"]; print(type(x)) #list / array
 print("posicao 1: ",x[0]);  print("posicao 2: ",x[1]);print("posicao 3: ",x[2]);print("posicao 4: ",x[3]);
+x=("carro","navio","aviao","trem"); print(type(x)) # tuple nao se altera o conteudo se usa () e list altera o conteudo usa []
+x=range (0,10,1); print(type(x)) # range cria um list nesse caso de 0 a 10 de 1 em 1
+x={
+    "terrestre":"carro",
+    "maritimo":"navio",
+    "aereo":"aviao"
+}; print(type(x)) #dict
+print("Valores",x["terrestre"],x['maritimo'],x['aereo'])
+x={5,7,8,6,4,5,7,2,1,1,3,3,6,8,5};print(type(x)) #set
+print((x))
+x=frozenset({5,7,8,6,4,5,7,2,1,1,3,3,6,8,5});print(type(x)) #bloqueia o set
