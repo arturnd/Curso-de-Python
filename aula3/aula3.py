30a6ce2b8b2653b40b71edc6cf02eeb50155e120
arturnd <arturrepresentacao@gmail.com>
2021-10-23 13:02:09 -0300

    modificado os arquivos aula3 aula4.py

--- aula3/aula3.py
+++ aula3/aula3.py
@@ -1,7 +1,25 @@
 num1=num2=res=0
-canal="CBF Cursos"
+canal1="CBF Cursos"
 
 def cn():
+    global canal
+    canal="cursos castro"
     print(canal)
-    
-cn ()      
+    print("\tVariavel Global, nome da variavel canal")
+cn ()
+
+def cn():
+    print(canal1)
+    print("\tVariavel Local nome da variavel canal1")
+cn()
+
+
+def cn():
+    global canal2
+    canal2 = "castro"
+    print(canal2)
+print("\n\tAqui nao foi exibido castro porque a cn() nao foi fechado")
+
+
+
+
--- aula4/aula4.py
+++ aula4/aula4.py
@@ -1,6 +1,12 @@
 
-hj=5
-ho= int(6)
+j=5
+k= input ("Digite um valor aqui: ")
+print("Variavel j inteiro: ",(j),type(j))
+print("Digitado :",k)
+print("Variavel k string ",(type(k)))
 
-print(type(hj))
-print(type(ho))
+x=1;print(type(x)) #inteiro
+x="1";print(type(x)) #string
+x=1.5;print(type(x)) #float
+X=True;print(type(x)) #bool
+n1=3;n2=6;x=complex(n1,n2);print(type(x))#complexo
