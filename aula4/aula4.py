ca9d439a32173b189ccf15923515cc514c7f36c8
arturnd <arturrepresentacao@gmail.com>
2021-10-23 13:47:06 -0300

    Arquivo aula4.py modificado

--- aula4/aula4.py
+++ aula4/aula4.py
@@ -5,8 +5,11 @@ print("Variavel j inteiro: ",(j),type(j))
 print("Digitado :",k)
 print("Variavel k string ",(type(k)))
 
-x=1;print(type(x)) #inteiro
-x="1";print(type(x)) #string
-x=1.5;print(type(x)) #float
-X=True;print(type(x)) #bool
-n1=3;n2=6;x=complex(n1,n2);print(type(x))#complexo
+x=1; print(type(x)) #inteiro
+x="1"; print(type(x)) #string
+x=1.5; print(type(x)) #float
+x=True; print(type(x)) #bool
+n1=3;n2=6;x=complex(n1,n2); print(type(x))#complexo
+print("Parte Real: ",(x.real));print('Parte Imag: ',(x.imag))
+x=["carro","navio","aviao","trem"]; print(type(x)) #list / array
+print("posicao 1: ",x[0]);  print("posicao 2: ",x[1]);print("posicao 3: ",x[2]);print("posicao 4: ",x[3]);
